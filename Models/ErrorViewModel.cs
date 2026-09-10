5c6d52c1767682747bce9f6d7c6ce0fc148c4915
Subham-techk <suhhamvishwakarma@gmail.com>
2026-09-10 21:06:22 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NEW_AI_PROJECT_USERINTERFACE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
